76818e53a1b34d43bde238d7b9ea4adedd12f0a1
jabich0 <134058178+jabich0@users.noreply.github.com>
2023-06-15 22:42:57 +0200

    Update Usuario.java

--- Juego_BINGO/src/Usuario.java
+++ Juego_BINGO/src/Usuario.java
@@ -1,4 +1,44 @@
-
 public class Usuario {
+    private String nombre;
+    private String apellidos;
+    private String correoElectronico;
+    private String contrasena;
+    private int edad;
 
+    public Usuario(String nombre, String apellidos, String correoElectronico, String contrasena, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.correoElectronico = correoElectronico;
+        this.contrasena = contrasena;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    
+    public String toString() {
+        return "Nombre: " + nombre +
+                "\nApellidos: " + apellidos +
+                "\nCorreo electrónico: " + correoElectronico +
+                "\nContraseña: " + contrasena +
+                "\nEdad: " + edad;
+    }
 }
